8f4416a31bbfbcd35124ffe0dc6581b60ab0da37
JorgeRodriguez1998 <jorgerodriguez5tos@gmail.com>
2022-04-30 13:45:35 -0500

    Updating User.test.js

---
+++ test/models/User.test.js
@@ -0,0 +1,20 @@
+const User = require('./app/models/User');
+
+describe('Unit Tests for User class', () => {
+
+    test('Create an User object', () => {
+        //Calling the code that we are going to use in the app
+        const user = new User(1, "JorgeRodriguez1998", "Jorge", "Bio", "dateCreated",
+            "lastUpdated");
+
+        //Here you validate the output.
+        //This is a comparison that will set the value on the left
+        //equal to the value on the right (expected value)
+        expect(user.id).toBe(1);
+        expect(user.username).toBe("JorgeRodriguez1998");
+        expect(user.name).toBe("Jorge");
+        expect(user.bio).toBe("Bio");
+        expect(user.dateCreated).toBe("dateCreated");
+        expect(user.lastUpdated).toBe("lastUpdated");
+    })
+})
