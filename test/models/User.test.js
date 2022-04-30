1a63f50a68b36d622d0a3dead611c5b74e05bf33
JorgeRodriguez1998 <jorgerodriguez5tos@gmail.com>
2022-04-30 15:00:41 -0500

    Adding requirements

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,8 +4,7 @@ describe('Unit Tests for User class', () => {
 
     test('Create an User object', () => {
         //Calling the code that we are going to use in the app
-        const user = new User(1, "JorgeRodriguez1998", "Jorge", "Bio", "dateCreated",
-            "lastUpdated");
+        const user = new User(1, "JorgeRodriguez1998", "Jorge", "Bio");
 
         //Here you validate the output.
         //This is a comparison that will set the value on the left
@@ -14,7 +13,11 @@ describe('Unit Tests for User class', () => {
         expect(user.username).toBe("JorgeRodriguez1998");
         expect(user.name).toBe("Jorge");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdated).toBe("lastUpdated");
+    })
+
+    test('Dates in user attributes', () => {
+        const user = new User(1, "JorgeRodriguez1998", "Jorge", "Bio");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
     })
 })
